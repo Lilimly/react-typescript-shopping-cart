940a22396c13cc02324d116ab1747171875be527
Lilimly <aureliemlynarz@gmail.com>
2021-03-30 12:15:00 +0200

    Add total cart price

--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    
+    display: flex;
+    justify-conten: space-between;
+    font-family: Arial, helvetica, sans-serif;
+    padding: 20px;
+    border-bottom: 1px solid lightblue;
+
+    div {
+        flex: 1;
+    }
+
+    .information, 
+    .buttons {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    img {
+        max-width: 120px;
+        object-fit: cover;
+        margin-left: 40px;
+    }
 `;
